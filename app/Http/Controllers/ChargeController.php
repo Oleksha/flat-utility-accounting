fca2b06f60e48fdf321ef282c0f60216f0591eeb
Oleksha <deryabino@gmail.com>
2025-12-13 10:49:40 +0400

    Доработал платежи со страницы квартиры

--- app/Http/Controllers/ChargeController.php
+++ app/Http/Controllers/ChargeController.php
@@ -17,8 +17,6 @@ class ChargeController extends Controller
 
     public function create()
     {
-        $apartments = Apartment::all();
-        $services = Service::all();
         return view('charges.create', [
             'apartments' => Apartment::all(),
             'services'   => Service::all(),
@@ -28,7 +26,7 @@ class ChargeController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate([
+        $data = $request->validate([
             'apartment_id' => 'required|exists:apartments,id',
             'service_id' => 'required|exists:services,id',
             'amount' => 'required|numeric|min:0',
@@ -36,8 +34,11 @@ class ChargeController extends Controller
             'comment' => 'nullable|string|max:255',
         ]);
 
-        Charge::create($request->all());
-        return redirect()->route('charges.index')->with('success', 'Начисление добавлено');
+        Charge::create($data);
+
+        return redirect()
+            ->route('apartments.show', $data['apartment_id'])
+            ->with('success', 'Начисление добавлено');
     }
 
     public function edit(Charge $charge)
@@ -51,7 +52,7 @@ class ChargeController extends Controller
 
     public function update(Request $request, Charge $charge)
     {
-        $request->validate([
+        $data = $request->validate([
             'apartment_id' => 'required|exists:apartments,id',
             'service_id' => 'required|exists:services,id',
             'amount' => 'required|numeric|min:0',
@@ -59,13 +60,20 @@ class ChargeController extends Controller
             'comment' => 'nullable|string|max:255',
         ]);
 
-        $charge->update($request->all());
-        return redirect()->route('charges.index')->with('success', 'Начисление обновлено');
+        $charge->update($data);
+
+        return redirect()
+            ->route('apartments.show', $data['apartment_id'])
+            ->with('success', 'Начисление обновлено');
     }
 
     public function destroy(Charge $charge)
     {
+        $apartmentId = $charge->apartment_id;
         $charge->delete();
-        return redirect()->route('charges.index')->with('success', 'Начисление удалено');
+
+        return redirect()
+            ->route('apartments.show', $apartmentId)
+            ->with('success', 'Начисление удалено');
     }
 }
